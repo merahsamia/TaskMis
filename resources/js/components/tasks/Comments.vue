e0f428414bb49193a2c0f6f7084fe21980a4a3b0
MERAH SAMIA <S.MERAH@OPGI35.COM>
2024-02-25 15:54:58 +0100

    create comments part 1

---
+++ resources/js/components/tasks/Comments.vue
@@ -0,0 +1,33 @@
+<template>
+     <!-- Modal -->
+        <div class="modal fade" id="commentsModal" tabindex="-1" aria-labelledby="commentsModalLabel"
+                        aria-hidden="true">
+            <div class="modal-dialog modal-xl modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="commentsModalLabel">
+                            Comments List For {{ taskInfo.title }}
+                        </h5>
+
+                      
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+
+                    </div>
+                    <div class="modal-body">
+                      
+
+                    
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+</template>
--- resources/js/components/tasks/Tasks.vue
+++ resources/js/components/tasks/Tasks.vue
@@ -45,6 +45,7 @@
                                     <th>Description</th>
                                     <th>Assign To</th>
                                     <th>Status</th>
+                                    <th v-if="current_permissions.has('comments-read')">ِComments</th>
                                     <th v-if="current_permissions.has('tasks-update') || current_permissions.has('tasks-delete')">Actions</th>
                                 </tr>
                             </thead>
@@ -68,6 +69,11 @@
                                         <p v-if="task.progress == 0" class="text-danger">No Progress</p>
                                         <p v-if="task.progress > 0 && task.progress < 100" class="text-warning">Under Progress</p>
                                         <p v-if="task.progress == 100" class="text-success">Completed</p>
+                                    </td>
+                                    <td v-if="current_permissions.has('comments-read')">
+                                        <button type="button" class="btn btn-secondary" @click="showComments(task)">
+                                                <i class="fa fa-comment"></i>
+                                        </button>                                   
                                     </td>
                                     
                                     <td v-if="current_permissions.has('tasks-update') || current_permissions.has('tasks-delete')">
@@ -198,6 +204,9 @@
                         </div>
                     </div>
 
+
+                    <Comments :taskInfo="taskInfo"/>
+
                 </div>
             </div>
         </div>
@@ -208,11 +217,13 @@
 
 <script>
 import Show from './Show.vue';
+import Comments from './Comments.vue';
 
 export default {
 
     components: {
-    Show
+    Show,
+    Comments,
     },
 
     data() {
@@ -355,6 +366,13 @@ export default {
 
         searchTask() {
             this.$store.dispatch('searchTask', this.searchData)
+
+        },
+
+        showComments(task){
+            this.taskInfo = task
+            $('#commentsModal').modal('show')
+
 
         },
 
